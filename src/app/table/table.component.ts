9edaa819f7e9b91244f4b4083a911717db76350e
Jack Koppa <hello@jackkoppa.com>
2018-07-30 22:33:16 -0400

    Create working fixed vertical, scrolling horizontal header

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 import { TableService } from './table.service';
 import {
@@ -10,9 +10,13 @@ import {
   selector: 'att-table',
   templateUrl: './table.component.html'
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, AfterViewInit {
+  @ViewChild('displayHeader') displayHeader: ElementRef;
+  @ViewChildren('displayHeaderItem') displayHeaderItem: QueryList<ElementRef>;
+  @ViewChildren('hiddenHeaderItem') hiddenHeaderItem: QueryList<ElementRef>;
   public data: OriginalDataShape[] = [];
   public dataHeadersMap: {}[] = [];
+  private lastScrollX: number = 0;
 
   constructor(private tableService: TableService) {
     this.dataHeadersMap = Object.keys(OriginalDataToHeadersMap)
@@ -22,7 +26,27 @@ export class TableComponent implements OnInit {
 
   ngOnInit() {
     this.tableService.getSampleData()
-      .subscribe(response => this.data = <OriginalDataShape[]>response);
+      .subscribe(response => {
+        this.data = <OriginalDataShape[]>response;
+      });
+    window.onscroll = (e) => {
+      if (this.lastScrollX !== window.scrollX) {
+        this.displayHeader.nativeElement.style.left = `${-window.scrollX}px`;
+        this.lastScrollX = window.scrollX;
+      }
+    }
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      const hiddenHeaderItems = this.hiddenHeaderItem.toArray();
+      const displayHeaderItems = this.displayHeaderItem.toArray();
+      for (let i = 0; i < hiddenHeaderItems.length; i++) {
+        console.log('scrollWidth:' + hiddenHeaderItems[i].nativeElement.scrollWidth);
+        displayHeaderItems[i].nativeElement.style.minWidth = `${hiddenHeaderItems[i].nativeElement.getBoundingClientRect().width}px`;
+      }
+    }, 2000)
+    // setTimeout(() => this.hiddenHeader.forEach(header => console.log(header.nativeElement.scrollWidth)), 5000);
   }
 
 }
