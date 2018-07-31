da530e9822ae51b6065a5faaee892616340ea1df
Jack Koppa <hello@jackkoppa.com>
2018-07-30 23:03:58 -0400

    Create working selector for number of rows per page

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -8,19 +8,27 @@ import {
 
 @Component({
   selector: 'att-table',
-  templateUrl: './table.component.html'
+  templateUrl: './table.component.html',
+  providers: [{provide: Window, useValue: window}]  // maintain DI to avoid direct reference to `window`,
+                                                    // thus allowing for testing environments without `window`
 })
 export class TableComponent implements OnInit {
   @ViewChild('displayHeader') displayHeader: ElementRef;
   @ViewChildren('displayHeaderItem') displayHeaderItem: QueryList<ElementRef>;
   @ViewChildren('hiddenHeaderItem') hiddenHeaderItem: QueryList<ElementRef>;
   public data: OriginalDataShape[] = [];
+  public displayedData: OriginalDataShape[] = [];
   public dataHeadersMap: {}[] = [];
+  public currentPage: number = 1;
+
+  private displayedRows: number = 25;
   private lastScrollX: number = 0;
+
 
   constructor(
     private tableService: TableService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private window: Window
   ) {
     this.dataHeadersMap = Object.keys(OriginalDataToHeadersMap)
       .map(prop => { return { value: prop, display: OriginalDataToHeadersMap[prop] } });
@@ -31,18 +39,30 @@ export class TableComponent implements OnInit {
     this.tableService.getSampleData()
       .subscribe(response => {
         this.data = <OriginalDataShape[]>response;
-        this.cdr.detectChanges();
-        const hiddenHeaderItems = this.hiddenHeaderItem.toArray();
-        const displayHeaderItems = this.displayHeaderItem.toArray();
-        for (let i = 0; i < hiddenHeaderItems.length; i++) {
-          displayHeaderItems[i].nativeElement.style.minWidth = `${hiddenHeaderItems[i].nativeElement.getBoundingClientRect().width}px`;
-        }
+        this.changeRows(this.displayedRows);
+        this.calculateHeaderWidths();
       });
-    window.onscroll = (e) => {
-      if (this.lastScrollX !== window.scrollX) {
-        this.displayHeader.nativeElement.style.left = `${-window.scrollX}px`;
-        this.lastScrollX = window.scrollX;
+    this.window.onscroll = (e) => {
+      if (this.lastScrollX !== this.window.scrollX) {
+        this.displayHeader.nativeElement.style.left = `${-this.window.scrollX}px`;
+        this.lastScrollX = this.window.scrollX;
       }
+    }
+  }
+
+  changeRows(newRows: number): void {
+    const firstItem = this.displayedRows * (this.currentPage - 1);
+    this.displayedRows = newRows;
+    this.displayedData = this.data.slice(firstItem, firstItem + this.displayedRows);
+    this.calculateHeaderWidths();
+  }
+
+  calculateHeaderWidths(): void {
+    this.cdr.detectChanges();
+    const hiddenHeaderItems = this.hiddenHeaderItem.toArray();
+    const displayHeaderItems = this.displayHeaderItem.toArray();
+    for (let i = 0; i < hiddenHeaderItems.length; i++) {
+      displayHeaderItems[i].nativeElement.style.minWidth = `${hiddenHeaderItems[i].nativeElement.getBoundingClientRect().width}px`;
     }
   }
 }
